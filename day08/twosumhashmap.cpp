0378c11da1e01864c600034cde672b26e9dbbc8e
yogeshkori01 <ykkorikori@gmail.com>
2026-08-24 23:30:07 +0530

    Add Day 8 Two Sum solution

---
+++ day08/twosumhashmap.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int required = target - nums[i];
+
+        if (mp.find(required) != mp.end()) {
+            return {mp[required], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = twoSum(nums, target);
+
+    cout << "Indices: " << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
